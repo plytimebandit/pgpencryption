7b7e4e8fb250f4ce84fb2bba2da6e3d847327342
plytimebandit <spallek.kj@gmail.com>
2017-04-01 10:22:35 +0200

    Refactoring of adding provider

--- src/main/java/org/plytimebandit/tools/pgpencryption/AppModule.java
+++ src/main/java/org/plytimebandit/tools/pgpencryption/AppModule.java
@@ -9,9 +9,8 @@ import com.google.inject.AbstractModule;
 public class AppModule extends AbstractModule {
 
     AppModule() {
-        BouncyCastleProvider provider = new BouncyCastleProvider();
-        if (Security.getProvider(provider.getName()) == null) {
-            Security.addProvider(provider);
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
         }
     }
 
